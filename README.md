5f18769dbbd8fa797e9de95f986ad07bc2eb1c90
Cillian de Róiste <goibhniu@fsfe.org>
2014-12-11 15:36:25 +0000

    Can add, remove items and submit the values to the action URL

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ Add a value to a "shopping cart" and process this "shopping cart".
 
 This can be used to implement a bulk download feature e.g.
 
-    <a class="pat-shopping-cart" data-pat-shopping-cart="uid 0d599f0ec05c3bda8c3b8a68c32a1b47;">
+    <a class="pat-shopping-cart" data-pat-shopping-cart="id: 0d599f0ec05c3bda8c3b8a68c32a1b47">
       Add this item for download
     </a>
 
-    <a class="pat-shopping-cart" data-pat-shopping-cart="action download">
-      You have 7 items selected for download. Download them here.
+When an item is added to the cart, the element is given the .in-cart css class.
+
+A .pat-shopping-cart element is also used for processing the cart.
+A list of the id values is submitted to the action URL using the POST method as a comma separated string.
+
+    <a class="pat-shopping-cart" data-pat-shopping-cart="action: /download-as-zip">
+      Download them here.
     </a>
+
+The shopping cart action element gets the .has-items css class when there are items in the cart, and .is-empty otherwise.
--- src/pat-shopping-cart.js
+++ src/pat-shopping-cart.js
@@ -10,46 +10,114 @@
             return factory.apply(this, arguments);
         });
     } else {
-        // A module loader is not available. In this case, we need the
-        // patterns library to be available as a global variable "patterns"
         factory(root.patterns, root.patterns.Parser, root.patterns.store);
     }
 }(this, function(registry, Parser, store) {
     "use strict";
 
-    // We instantiate a new Parser instance, which will parse HTML markup
-    // looking for configuration settings for this pattern.
-    //
-    // This example pattern's name is pat-colorchanger. It is activated on a DOM
-    // element by giving the element the HTML class "pat-colorchanger".
-    //
-    // The pattern can be configured by specifying an HTML5 data attribute
-    // "data-pat-colorchanger" which contains the configuration parameters
-    // Only configuration parameters specified here are valid.
-    //
-    // For example:
-    //      <p class="pat-colorchanger" data-pat-colorchanger="color: blue">Hello World</p>
-
-    var parser = new Parser("example");
-    parser.add_argument("color", "red"); // A configuration parameter and its default value.
+    var parser = new Parser("shopping-cart");
+    parser.add_argument("id");
+    parser.add_argument("action");
 
     // We now create an object which encapsulates the pattern's methods
-    var example= {
-        name: "example",
-        trigger: ".pat-colorchanger",
+    var shoppingCart= {
+        name: "shopping-cart",
+        trigger: ".pat-shopping-cart",
+        storage: store.local("pat-shopping-cart"),
 
-        init: function patExampleInit($el, opts) {
-            var options = parser.parse($el, opts);  // Parse the DOM element to retrieve the
-            // configuration settings.
-            setTimeout($.proxy(function () {
-                this.setColor($el, options);
-            }, this), 3000);
+        init: function($el, opts) {
+            var options = parser.parse($el, opts);
+            $el.click($.proxy(function() {
+                if ("action" in options) {
+                    this.processCart(options);
+                } else if ("id" in options) {
+                    if (this.isAddedToCart(options)) {
+                        this.removeFromCart($el, options);
+                    }
+                    else {
+                        this.addToCart($el, options);
+                    }
+                }
+            }, this));
+            if ("action" in options) {
+                if (this.cartIsEmpty()) {
+                    $el.addClass("is-empty");
+                } else {
+                    $el.addClass("has-items");
+                }
+            }
+            if ("id" in options && this.isAddedToCart(options)) $el.addClass("in-cart");
         },
 
-        setColor: function patExampleSetColor($el, options) {
-            $el.css("color", options.color);
+        addToCart: function($el, options) {
+            var downloads = this.storage.get("downloads");
+            if (downloads === null) {
+                this.storage.set("downloads", [options.id]);
+                $(".pat-shopping-cart.is-empty").each(function(index) {
+                    $(this).removeClass("is-empty");
+                    $(this).addClass("has-items");
+                });
+            } else {
+                downloads.push(options.id);
+                this.storage.set("downloads", downloads);
+            }
+            $el.addClass("in-cart");
+        },
+
+        removeFromCart: function($el, options) {
+            var downloads = this.storage.get("downloads");
+            if (downloads.length === 1) {
+                this.emptyCart();
+            } else {
+                var itemIndex = downloads.indexOf(options.id);
+                downloads.splice(itemIndex, 1);
+                this.storage.set("downloads", downloads);
+            }
+            $el.removeClass("in-cart");
+        },
+
+        isAddedToCart: function(options) {
+            var downloads = this.storage.get("downloads");
+            if (downloads === null) {
+                return false;
+            } else if (downloads.indexOf(options.id) > -1) {
+                return true;
+            } else return false;
+        },
+
+        emptyCart: function() {
+            this.storage.remove("downloads");
+            $(".pat-shopping-cart.has-items").each(function(index) {
+                $(this).removeClass("has-items");
+                $(this).addClass("is-empty");
+            });
+        },
+
+        cartIsEmpty: function($el, options) {
+            console.log("empty");
+            return (this.storage.get("downloads") === null);
+        },
+
+        processCart: function(options) {
+            var action = options.action;
+            var downloads = this.storage.get("downloads");
+
+            // Note: we assume success and clear the cart
+            this.emptyCart();
+            $("#shopping-cart").remove();
+            $(".pat-shopping-cart.in-cart").each(function(index) {
+                $(this).removeClass("in-cart");
+            });
+
+
+            $("body").append(
+                '<form id="shopping-cart" action="'+action+'" method="post">'+
+                    '<input name="shopping-cart" type="hidden" value="'+downloads+'"/>'+
+                    '</form>'
+            );
+            $("#shopping-cart").submit();
         }
+
     };
-    // Finally, we register the pattern object in the registry.
-    registry.register(example);
+    registry.register(shoppingCart);
 }));

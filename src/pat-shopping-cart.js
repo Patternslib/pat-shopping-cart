fb81e3416d2ff5a9a47f26dea71867825a116f9f
Cillian de Róiste <deroiste@syslab.com>
2016-03-08 15:11:06 +0100

    Add the "keep" and "patterns" options
    "keep" allows an action to be performed on the cart without clearing the
    elements.
    
    "patterns" allows patterns to be passed on to the cart e.g. pat-modal.
    
    Also add checkbox support for adding items to the cart and disable
    shopping-cart action elements when the cart is empty.

--- src/pat-shopping-cart.js
+++ src/pat-shopping-cart.js
@@ -16,8 +16,10 @@
     "use strict";
 
     var parser = new Parser("shopping-cart");
-    parser.add_argument("id");
     parser.add_argument("action");
+    parser.add_argument("id");
+    parser.add_argument("keep");
+    parser.add_argument("patterns");
 
     // We now create an object which encapsulates the pattern's methods
     var shoppingCart= {
@@ -31,67 +33,84 @@
                 if ("action" in options) {
                     if (options.action == "empty") {
                         this.emptyCart();
-                    } else this.processCart(options);
+                    } else this.processCart($el, options);
                 } else if ("id" in options) {
                     if (this.isAddedToCart(options)) {
                         this.removeFromCart($el, options);
                     }
-                    else {
-                        this.addToCart($el, options);
-                    }
-                }
+                    else this.addToCart($el, options);
+                };
             }, this));
             if ("action" in options) {
                 if (this.cartIsEmpty()) {
                     $el.addClass("is-empty");
+                    $el.attr("disabled", "disabled");
                 } else {
                     $el.addClass("has-items");
-                }
-            }
-            if ("id" in options && this.isAddedToCart(options)) $el.addClass("in-cart");
+                };
+            };
+            if ("id" in options && this.isAddedToCart(options)) {
+                $el.addClass("in-cart");
+                if ($el.attr("type") == "checkbox") {
+                    $el.attr("checked", "checked");
+                    $el.parentsUntil("lable").each(function(index, elem) {
+                        $(elem).removeClass("unchecked").addClass("checked");
+                    });
+                };
+            };
+            if ("keep" in options && options.keep.toLowerCase() == 'true') {
+                options.keep = true;
+            } else options.keep = false;
+            if (!("patterns" in options)) options.patterns = "";
         },
 
         addToCart: function($el, options) {
-            var downloads = this.storage.get("downloads");
-            if (downloads === null) {
-                this.storage.set("downloads", [options.id]);
+            var cart = this.storage.get("cart");
+            if (cart === null) {
+                this.storage.set("cart", [options.id]);
                 $(".pat-shopping-cart.is-empty").each(function(index) {
-                    $(this).removeClass("is-empty");
-                    $(this).addClass("has-items");
+                    $(this).removeClass("is-empty").addClass("has-items");
+                    $(this).removeAttr("disabled");
                 });
             } else {
-                downloads.push(options.id);
-                this.storage.set("downloads", downloads);
+                cart.push(options.id);
+                this.storage.set("cart", cart);
             }
             $el.addClass("in-cart");
         },
 
         removeFromCart: function($el, options) {
-            var downloads = this.storage.get("downloads");
-            if (downloads.length === 1) {
+            var cart = this.storage.get("cart");
+            if (cart.length === 1) {
                 this.emptyCart();
             } else {
-                var itemIndex = downloads.indexOf(options.id);
-                downloads.splice(itemIndex, 1);
-                this.storage.set("downloads", downloads);
+                var itemIndex = cart.indexOf(options.id);
+                cart.splice(itemIndex, 1);
+                this.storage.set("cart", cart);
             }
             $el.removeClass("in-cart");
+            if ($el.attr("type") == "checkbox") {
+                $el.parentsUntil("lable").each(function(index, elem) {
+                    $(elem).removeClass("checked").addClass("unchecked");
+                });
+            };
         },
 
         isAddedToCart: function(options) {
-            var downloads = this.storage.get("downloads");
-            if (downloads === null) {
+            var cart = this.storage.get("cart");
+            if (cart === null) {
                 return false;
-            } else if (downloads.indexOf(options.id) > -1) {
+            } else if (cart.indexOf(options.id) > -1) {
                 return true;
             } else return false;
         },
 
         emptyCart: function() {
-            this.storage.remove("downloads");
+            this.storage.remove("cart");
             $(".pat-shopping-cart.has-items").each(function(index) {
                 $(this).removeClass("has-items");
                 $(this).addClass("is-empty");
+                $(this).attr("disabled", "disabled");
             });
             $(".pat-shopping-cart.in-cart").each(function(index) {
                 $(this).removeClass("in-cart");
@@ -100,23 +119,22 @@
 
         cartIsEmpty: function($el, options) {
             console.log("empty");
-            return (this.storage.get("downloads") === null);
+            return (this.storage.get("cart") === null);
         },
 
-        processCart: function(options) {
-            var action = options.action;
-            var downloads = this.storage.get("downloads");
+        processCart: function($el, options) {
+            var cart = this.storage.get("cart");
 
-            // Note: we assume success and clear the cart
-            this.emptyCart();
+            if (!options.keep) this.emptyCart();
 
-            $("#shopping-cart").remove();
-            $("body").append(
-                '<form id="shopping-cart" action="'+action+'" method="post">'+
-                    '<input name="shopping-cart" type="hidden" value="'+downloads+'"/>'+
-                    '</form>'
+            var form = $(
+                '<form action="'+options.action+'" class="'+options.patterns+'" method="post">'+
+                    '<input name="shopping-cart" type="hidden" value="'+cart+'"/>'+
+                '</form>'
             );
-            $("#shopping-cart").submit();
+
+            registry.scan(form);
+            form.submit();
         }
 
     };

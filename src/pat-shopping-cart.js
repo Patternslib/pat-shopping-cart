4f7394bcb5b58cd224e72afc1e619dd2b53daa38
Cillian de Róiste <deroiste@syslab.com>
2016-03-18 14:20:13 +0100

    Chrome bugfix: click on a button before disabling it
    This prevented forms from being submitted when a button also cleared the
    cart.

--- src/pat-shopping-cart.js
+++ src/pat-shopping-cart.js
@@ -122,6 +122,7 @@
             $(".pat-shopping-cart.has-items").each(function(index) {
                 $(this).removeClass("has-items");
                 $(this).addClass("is-empty");
+                $(this).click();
                 $(this).attr("disabled", "disabled");
             });
             $(".pat-shopping-cart.in-cart").each(function(index) {
